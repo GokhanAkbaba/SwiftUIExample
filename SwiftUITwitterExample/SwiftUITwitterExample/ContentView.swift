2384b9d09eaaa9d52ef76756cf2ba07473200584
GokhanAkbaba <gokhanakbaba1907@gmail.com>
2022-03-08 01:31:08 +0300

    Başlangıç
    Proje kurulumuna başlandı

---
+++ SwiftUITwitterExample/SwiftUITwitterExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITwitterExample
+//
+//  Created by Dijital Dönüşüm on 8.03.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUITwitterExample/SwiftUITwitterExample/SwiftUITwitterExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITwitterExampleApp.swift
+//  SwiftUITwitterExample
+//
+//  Created by Dijital Dönüşüm on 8.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITwitterExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
